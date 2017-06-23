7734d8da0c42e1b7a56e1596a2587adb8eb3b989
Manoj Khande <manoj.31khande@gmail.com>
2017-06-23 19:29:05 +0530

    Added comments

--- Calculator.php
+++ Calculator.php
@@ -1,12 +1,26 @@
 <?php
+/*
+    Class: Calculator
+*/
 class Calculator
 {
- 
+	/**
+	 * Function add
+	 * Use to add two numbers
+	 * @param (int) $a and $b
+	 * @return (int)
+	 */ 
     public function add($a, $b)
     {
         return $a + $b;
     }
 
+    /**
+	 * Function isEvenNumber
+	 * Use to check is number ever or not
+	 * @param (int) $number
+	 * @return (bool) 
+	 */ 
     public function isEvenNumber($number){
 		if ($number % 2 == 0) {
 		  return true;
@@ -17,4 +31,5 @@ class Calculator
     }
  
 }
+// Class End
 ?>
--- CalculatorTest.php
+++ CalculatorTest.php
@@ -1,32 +1,39 @@
 <?php
 require 'Calculator.php';
- 
+ /*
+    Class: CalculatorTests
+*/
 class CalculatorTests extends PHPUnit_Framework_TestCase
 {
     private $calculator;
- 
+    
+    // Function to initiate class Calculator
     protected function setUp()
     {
         $this->calculator = new Calculator();
     }
- 
+    
+    //Function to unset calculator
     protected function tearDown()
     {
         $this->calculator = NULL;
     }
- 
+    
+    // function to test Add
     public function testAdd()
     {
         $result = $this->calculator->add(1, 2);
         $this->assertEquals(3, $result);
     }
 
+    // function to test testisEvenNumber
     public function testisEvenNumber()
     {
         $result = $this->calculator->isEvenNumber(2);
         $this->assertTrue($result);
     }
 
+    // Function to test testisEvenNumber fail condition
     public function testisEvenNumberFail()
     {
         $result = $this->calculator->isEvenNumber(3);
